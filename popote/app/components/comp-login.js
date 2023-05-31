e0f315f44007bbf0638623962fb7eb4e69c2999b
MMre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-05-31 20:27:16 +0300

    Initilize trial deployment removed empty file

--- popote/app/components/comp-login.js
+++ popote/app/components/comp-login.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
 
-export default function Login(){
-    return(
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
         <main>
             <h1>Login</h1>
             <input
                 type="email"
                 placeholder="Email"
                 value={email}
+                onChange={(e) => setEmail(e.target.value)}
             />
             <input
                 type="password"
                 placeholder="Password"
                 value={password}
+                onChange={(e) => setPassword(e.target.value)}
             />
             <button>Login</button>
         </main>
-    )
+    );
 }
