37b317a441bb8fce522cd647c92391238087f430
MMre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-05-31 19:43:25 +0300

    Initilize trial deployment

--- popote/pages/dashboard/navbar.js
+++ popote/pages/dashboard/navbar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { RiDashboardLine, RiUserLine, RiSettingsLine } from 'react-icons/ri';
 import { AiOutlineUsergroupAdd } from 'react-icons/ai';
 import { FiDatabase } from 'react-icons/fi';
+import Link from 'next/link'; // Import the Link component
 import './styles.css';
 import Logo from './logo';
 
@@ -16,10 +17,13 @@ export default function DashboardNavbar() {
         <nav className="bg-blue-500 lg:w-1/5">
             <div className="container mx-auto">
                 <div className="flex items-center justify-between h-16">
-                    <a className="text-white text-xl font-bold flex items-center" href="#">
-                        <Logo className="h-6 w-6 mr-2" />
-                        Popote
-                    </a>
+                    {/* Use Link instead of anchor element */}
+                    <Link href="/">
+                        <a className="text-white text-xl font-bold flex items-center">
+                            <Logo className="h-6 w-6 mr-2" />
+                            Popote
+                        </a>
+                    </Link>
                     <button
                         className="text-black focus:outline-none lg:hidden"
                         type="button"
@@ -40,41 +44,37 @@ export default function DashboardNavbar() {
                 >
                     <div className="lg:w-9/12" style={{ margin: '20px' }}>
                         <div className="flex flex-col h-screen">
-                            <a
-                                className="text-white flex items-center px-4 py-2 hover:bg-blue-700 border-b border-blue-500 rounded-md font-bold"
-                                href="/"
-                            >
-                                <RiDashboardLine className="mr-2" />
-                                Dashboard
-                            </a>
-                            <a
-                                className="text-white flex items-center px-4 py-2 hover:bg-blue-700 border-b border-blue-500 rounded-md"
-                                href="/accounts"
-                            >
-                                <AiOutlineUsergroupAdd className="mr-2" />
-                                Accounts
-                            </a>
-                            <a
-                                className="text-white flex items-center px-4 py-2 hover:bg-blue-700 border-b border-blue-500 rounded-md"
-                                href="/management"
-                            >
-                                <FiDatabase className="mr-2" />
-                                Management
-                            </a>
-                            <a
-                                className="text-white flex items-center px-4 py-2 hover:bg-blue-700 border-b border-blue-500 rounded-md"
-                                href="/users"
-                            >
-                                <RiUserLine className="mr-2" />
-                                Users
-                            </a>
-                            <a
-                                className="text-white flex items-center px-4 py-2 hover:bg-blue-700 border-b border-blue-500 rounded-md"
-                                href="/settings"
-                            >
-                                <RiSettingsLine className="mr-2" />
-                                Settings
-                            </a>
+                            {/* Use Link instead of anchor elements */}
+                            <Link href="/">
+                                <a className="text-white flex items-center px-4 py-2 hover:bg-blue-700 border-b border-blue-500 rounded-md font-bold">
+                                    <RiDashboardLine className="mr-2" />
+                                    Dashboard
+                                </a>
+                            </Link>
+                            <Link href="/accounts">
+                                <a className="text-white flex items-center px-4 py-2 hover:bg-blue-700 border-b border-blue-500 rounded-md">
+                                    <AiOutlineUsergroupAdd className="mr-2" />
+                                    Accounts
+                                </a>
+                            </Link>
+                            <Link href="/management">
+                                <a className="text-white flex items-center px-4 py-2 hover:bg-blue-700 border-b border-blue-500 rounded-md">
+                                    <FiDatabase className="mr-2" />
+                                    Management
+                                </a>
+                            </Link>
+                            <Link href="/users">
+                                <a className="text-white flex items-center px-4 py-2 hover:bg-blue-700 border-b border-blue-500 rounded-md">
+                                    <RiUserLine className="mr-2" />
+                                    Users
+                                </a>
+                            </Link>
+                            <Link href="/settings">
+                                <a className="text-white flex items-center px-4 py-2 hover:bg-blue-700 border-b border-blue-500 rounded-md">
+                                    <RiSettingsLine className="mr-2" />
+                                    Settings
+                                </a>
+                            </Link>
                         </div>
                     </div>
                 </div>
