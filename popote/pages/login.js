a479cc238aebaa3608e5636f2e510f3f5fa8c2a1
MMre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-05-31 20:34:13 +0300

    Initilize trial deployment removed login file

--- popote/pages/login.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Login from '@/app/components/comp-login'
-export default function login() {
-    return (
-            <main className="container">
-                <Login />
-            </main>
-    )
-}
